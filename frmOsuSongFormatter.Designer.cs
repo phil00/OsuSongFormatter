33a536c210e9f29d701221d470669d20f22febc7
Phil00 <phil00@hotmail.com>
2014-06-13 11:56:00 -0400

    v0.1
    Creation of the application

---
+++ frmOsuSongFormatter.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace OsuSongFormatter
+{
+    partial class frmOsuSongFormatter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSongFolder = new System.Windows.Forms.Label();
+            this.txtSongFolder = new System.Windows.Forms.TextBox();
+            this.btnSongFolder = new System.Windows.Forms.Button();
+            this.lblListOfSongs = new System.Windows.Forms.Label();
+            this.lbListOfSongs = new System.Windows.Forms.ListBox();
+            this.txtDestinationFolder = new System.Windows.Forms.TextBox();
+            this.btnDestinationFolder = new System.Windows.Forms.Button();
+            this.lblDestinationFolder = new System.Windows.Forms.Label();
+            this.btnTransferAndFormat = new System.Windows.Forms.Button();
+            this.pgbTransfert = new System.Windows.Forms.ProgressBar();
+            this.lbError = new System.Windows.Forms.ListBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.lblTransfert = new System.Windows.Forms.Label();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSongFolder
+            // 
+            this.lblSongFolder.AutoSize = true;
+            this.lblSongFolder.Location = new System.Drawing.Point(12, 9);
+            this.lblSongFolder.Name = "lblSongFolder";
+            this.lblSongFolder.Size = new System.Drawing.Size(84, 13);
+            this.lblSongFolder.TabIndex = 0;
+            this.lblSongFolder.Text = "Osu! song folder";
+            // 
+            // txtSongFolder
+            // 
+            this.txtSongFolder.Location = new System.Drawing.Point(12, 25);
+            this.txtSongFolder.Name = "txtSongFolder";
+            this.txtSongFolder.ReadOnly = true;
+            this.txtSongFolder.Size = new System.Drawing.Size(512, 20);
+            this.txtSongFolder.TabIndex = 1;
+            this.txtSongFolder.Click += new System.EventHandler(this.SelectSongFolder);
+            // 
+            // btnSongFolder
+            // 
+            this.btnSongFolder.Location = new System.Drawing.Point(530, 25);
+            this.btnSongFolder.Name = "btnSongFolder";
+            this.btnSongFolder.Size = new System.Drawing.Size(31, 20);
+            this.btnSongFolder.TabIndex = 2;
+            this.btnSongFolder.Text = "...";
+            this.btnSongFolder.UseVisualStyleBackColor = true;
+            this.btnSongFolder.Click += new System.EventHandler(this.SelectSongFolder);
+            // 
+            // lblListOfSongs
+            // 
+            this.lblListOfSongs.AutoSize = true;
+            this.lblListOfSongs.Location = new System.Drawing.Point(21, 120);
+            this.lblListOfSongs.Name = "lblListOfSongs";
+            this.lblListOfSongs.Size = new System.Drawing.Size(66, 13);
+            this.lblListOfSongs.TabIndex = 3;
+            this.lblListOfSongs.Text = "List of songs";
+            // 
+            // lbListOfSongs
+            // 
+            this.lbListOfSongs.FormattingEnabled = true;
+            this.lbListOfSongs.Location = new System.Drawing.Point(24, 137);
+            this.lbListOfSongs.Name = "lbListOfSongs";
+            this.lbListOfSongs.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.lbListOfSongs.Size = new System.Drawing.Size(537, 225);
+            this.lbListOfSongs.TabIndex = 4;
+            // 
+            // txtDestinationFolder
+            // 
+            this.txtDestinationFolder.Location = new System.Drawing.Point(12, 68);
+            this.txtDestinationFolder.Name = "txtDestinationFolder";
+            this.txtDestinationFolder.ReadOnly = true;
+            this.txtDestinationFolder.Size = new System.Drawing.Size(512, 20);
+            this.txtDestinationFolder.TabIndex = 5;
+            this.txtDestinationFolder.Click += new System.EventHandler(this.SelectDestinationFolder);
+            // 
+            // btnDestinationFolder
+            // 
+            this.btnDestinationFolder.Location = new System.Drawing.Point(530, 68);
+            this.btnDestinationFolder.Name = "btnDestinationFolder";
+            this.btnDestinationFolder.Size = new System.Drawing.Size(31, 20);
+            this.btnDestinationFolder.TabIndex = 6;
+            this.btnDestinationFolder.Text = "...";
+            this.btnDestinationFolder.UseVisualStyleBackColor = true;
+            this.btnDestinationFolder.Click += new System.EventHandler(this.SelectDestinationFolder);
+            // 
+            // lblDestinationFolder
+            // 
+            this.lblDestinationFolder.AutoSize = true;
+            this.lblDestinationFolder.Location = new System.Drawing.Point(12, 52);
+            this.lblDestinationFolder.Name = "lblDestinationFolder";
+            this.lblDestinationFolder.Size = new System.Drawing.Size(89, 13);
+            this.lblDestinationFolder.TabIndex = 7;
+            this.lblDestinationFolder.Text = "Destination folder";
+            // 
+            // btnTransferAndFormat
+            // 
+            this.btnTransferAndFormat.Location = new System.Drawing.Point(584, 137);
+            this.btnTransferAndFormat.Name = "btnTransferAndFormat";
+            this.btnTransferAndFormat.Size = new System.Drawing.Size(97, 53);
+            this.btnTransferAndFormat.TabIndex = 8;
+            this.btnTransferAndFormat.Text = "Transfer and format";
+            this.btnTransferAndFormat.UseVisualStyleBackColor = true;
+            this.btnTransferAndFormat.Click += new System.EventHandler(this.btnTransferAndFormat_Click);
+            // 
+            // pgbTransfert
+            // 
+            this.pgbTransfert.Location = new System.Drawing.Point(12, 589);
+            this.pgbTransfert.Name = "pgbTransfert";
+            this.pgbTransfert.Size = new System.Drawing.Size(861, 23);
+            this.pgbTransfert.TabIndex = 9;
+            // 
+            // lbError
+            // 
+            this.lbError.FormattingEnabled = true;
+            this.lbError.Location = new System.Drawing.Point(24, 401);
+            this.lbError.Name = "lbError";
+            this.lbError.Size = new System.Drawing.Size(537, 95);
+            this.lbError.TabIndex = 10;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(21, 385);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(64, 13);
+            this.lblError.TabIndex = 11;
+            this.lblError.Text = "List of errors";
+            // 
+            // lblTransfert
+            // 
+            this.lblTransfert.AutoSize = true;
+            this.lblTransfert.Location = new System.Drawing.Point(12, 573);
+            this.lblTransfert.Name = "lblTransfert";
+            this.lblTransfert.Size = new System.Drawing.Size(24, 13);
+            this.lblTransfert.TabIndex = 12;
+            this.lblTransfert.Text = "0/0";
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(715, 137);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(97, 53);
+            this.btnAbout.TabIndex = 13;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // frmOsuSongFormatter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 624);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.lblTransfert);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lbError);
+            this.Controls.Add(this.pgbTransfert);
+            this.Controls.Add(this.btnTransferAndFormat);
+            this.Controls.Add(this.lblDestinationFolder);
+            this.Controls.Add(this.btnDestinationFolder);
+            this.Controls.Add(this.txtDestinationFolder);
+            this.Controls.Add(this.lbListOfSongs);
+            this.Controls.Add(this.lblListOfSongs);
+            this.Controls.Add(this.btnSongFolder);
+            this.Controls.Add(this.txtSongFolder);
+            this.Controls.Add(this.lblSongFolder);
+            this.Name = "frmOsuSongFormatter";
+            this.Text = "Osu! Song Formatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSongFolder;
+        private System.Windows.Forms.TextBox txtSongFolder;
+        private System.Windows.Forms.Button btnSongFolder;
+        private System.Windows.Forms.Label lblListOfSongs;
+        private System.Windows.Forms.ListBox lbListOfSongs;
+        private System.Windows.Forms.TextBox txtDestinationFolder;
+        private System.Windows.Forms.Button btnDestinationFolder;
+        private System.Windows.Forms.Label lblDestinationFolder;
+        private System.Windows.Forms.Button btnTransferAndFormat;
+        private System.Windows.Forms.ProgressBar pgbTransfert;
+        private System.Windows.Forms.ListBox lbError;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label lblTransfert;
+        private System.Windows.Forms.Button btnAbout;
+    }
+}
+
---
+++ frmOsuSongFormatter.cs
@@ -0,0 +1,240 @@
+﻿//using DragonOgg.MediaPlayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TagLib;
+
+namespace OsuSongFormatter
+{
+    public partial class frmOsuSongFormatter : Form
+    {
+        private int _intNbSong = 0;
+        private int _intCurSong = 0;
+
+        private delegate void DeleguePgbTransfert();
+
+        private delegate void DelegueLblTransfert();
+
+        public frmOsuSongFormatter()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectSongFolder(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbdOsuSongsFolder = new FolderBrowserDialog();
+            DialogResult dgResult = fbdOsuSongsFolder.ShowDialog();
+
+            if (dgResult == DialogResult.OK)
+            {
+                txtSongFolder.Text = fbdOsuSongsFolder.SelectedPath;
+
+                String[] strSongsFolders = Directory.GetDirectories(fbdOsuSongsFolder.SelectedPath);
+
+                Array.Sort(strSongsFolders, StringComparer.CurrentCultureIgnoreCase);
+
+                String[] strSongsNameList = GetSongsName(strSongsFolders);
+
+                lbListOfSongs.Items.Clear();
+                lbListOfSongs.Items.AddRange(strSongsNameList);
+                lblTransfert.Text = "0/" + strSongsNameList.Length;
+                pgbTransfert.Maximum = strSongsNameList.Length;
+                pgbTransfert.Step = 1;
+            }
+        }
+
+        private void SelectDestinationFolder(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbdDestination = new FolderBrowserDialog();
+            DialogResult dgResult = fbdDestination.ShowDialog();
+
+            if (dgResult == DialogResult.OK)
+            {
+                txtDestinationFolder.Text = fbdDestination.SelectedPath;
+            }
+        }
+
+        private String[] GetSongsName(String[] strSongsFolders)
+        {
+            String[] strSongsNameList = new String[strSongsFolders.Length];
+
+            for (int i = 0; i < strSongsFolders.Length; i++)
+            {
+                strSongsNameList[i] = GetSongName(new DirectoryInfo(strSongsFolders[i]));
+            }
+
+            return strSongsNameList;
+        }
+
+        private void btnTransferAndFormat_Click(object sender, EventArgs e)
+        {
+            if (txtSongFolder.Text != "")
+            {
+                if (txtDestinationFolder.Text != "")
+                {
+                    Thread thTransfertAndFormat = new Thread(TransfertAndFormat);
+                    thTransfertAndFormat.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Please select the destination folder");
+                    btnDestinationFolder.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select the osu song folder!");
+                btnSongFolder.Focus();
+            }
+
+
+        }
+
+        private string GetSongName(DirectoryInfo diSongFolder)
+        {
+            string strSongName = "";
+            if (Char.IsNumber(diSongFolder.Name[0]))
+            {
+                int intSpacePos = diSongFolder.Name.IndexOf(' ');
+                if (intSpacePos != -1)
+                {
+                    strSongName = diSongFolder.Name.Substring(intSpacePos + 1);
+                }
+                else
+                {
+                    strSongName = diSongFolder.Name;
+                }
+            }
+            else
+            {
+                strSongName = diSongFolder.Name;
+            }
+            return strSongName;
+        }
+
+        private void TransfertAndFormat()
+        {
+            DirectoryInfo diOsuSongFolder = new DirectoryInfo(txtSongFolder.Text);
+            DirectoryInfo diDestinationFolder = new DirectoryInfo(txtDestinationFolder.Text);
+            DirectoryInfo[] diSongsFolderList = diOsuSongFolder.GetDirectories();
+            DeleguePgbTransfert dgPgbTransfertUpdate = delegate { this.pgbTransfert.PerformStep(); this.pgbTransfert.Refresh(); };
+            DelegueLblTransfert dgLblTransfertUpdate = delegate { this.lblTransfert.Text = _intCurSong + "/" + _intNbSong; ; this.lblTransfert.Refresh(); };
+            DeleguePgbTransfert dgPgbTransfertReset = delegate { this.pgbTransfert.Value = 0; this.pgbTransfert.Refresh(); };
+            DelegueLblTransfert dgLblTransfertReset = delegate { _intCurSong = 0; this.lblTransfert.Text = "0/" + _intNbSong; ; this.lblTransfert.Refresh(); };
+            _intNbSong = diSongsFolderList.Length;
+
+            this.Invoke(dgPgbTransfertReset);
+            this.Invoke(dgLblTransfertReset);
+
+            foreach (DirectoryInfo diSongFolder in diSongsFolderList)
+            {
+                bool blnImgFound = false;
+                IPicture ipImg = new Picture();
+                if (diSongFolder.Name != "tutorial")
+                {
+                    foreach (FileInfo fiFileInFolder in diSongFolder.GetFiles())
+                    {
+                        if (fiFileInFolder.Extension == ".jpg")
+                        {
+                            blnImgFound = true;
+                            ipImg = new Picture(fiFileInFolder.FullName);
+                        }
+
+                        if (fiFileInFolder.Extension == ".mp3" || fiFileInFolder.Extension == ".ogg")
+                        {
+                            if (!new FileInfo(diDestinationFolder.FullName + "\\" + GetSongName(diSongFolder) + fiFileInFolder.Extension).Exists)
+                            {
+                                String strSongName = GetSongName(diSongFolder);
+                                String strPath = diDestinationFolder.FullName + "\\" + strSongName + fiFileInFolder.Extension;
+                                System.IO.File.Copy(fiFileInFolder.FullName, strPath, false);
+
+                                String[] strArtistAndTitle = strSongName.Split(new string[1] { " - " }, StringSplitOptions.None);
+                                String strAlbum = "Osu!";
+                                String[] strPerformers = new String[1] { strArtistAndTitle[0] };
+
+                                if (fiFileInFolder.Extension == ".mp3")
+                                {
+                                    TagLib.File fMpegSong = TagLib.Mpeg.File.Create(strPath);
+                                    TagEditor(fMpegSong, strAlbum, strPerformers, strArtistAndTitle, blnImgFound, ipImg);
+
+                                }
+                                else
+                                {
+                                    if (fiFileInFolder.Extension == ".ogg")
+                                    {
+                                        TagLib.File fOggSong = TagLib.Ogg.File.Create(strPath);
+                                        TagEditor(fOggSong, strAlbum, strPerformers, strArtistAndTitle, blnImgFound, ipImg);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                _intCurSong++;
+                this.Invoke(dgLblTransfertUpdate);
+                this.Invoke(dgPgbTransfertUpdate);
+            }
+            MessageBox.Show("Operation terminated successfully!");
+        }
+
+        private void TagEditor(TagLib.File fSong, String strAlbum, String[] strPerformers, String[] strArtistAndTitle, bool blnImgFound, IPicture ipImg)
+        {
+            try
+            {
+                fSong.Tag.Album = strAlbum;
+                fSong.Tag.Performers = strPerformers;
+                if (strArtistAndTitle.Length == 2)
+                {
+                    fSong.Tag.Title = strArtistAndTitle[1];
+                }
+                else
+                {
+                    fSong.Tag.Title = strArtistAndTitle[0];
+                }
+                if (blnImgFound)
+                {
+                    fSong.Tag.Pictures = new IPicture[] { ipImg };
+                }
+                fSong.Save();
+            }
+            catch (Exception ex)
+            {
+                lbError.Items.Add(ex.ToString());
+            }
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This application format and copy songs from your Osu! repository for you to add to your library and have good title and tags.\n" +
+                            "Original Osu .mp3 and .ogg files don't have the good title and they don't have any tags.\n" +
+                            "This application function as follow :\n" +
+                            "1-You select the Osu! song folder.\n" +
+                            "2-You select the destination where you want to copy your songs.\n" +
+                            "3-The title of all the song will appear is the listbox.\n" +
+                            "   (The titles are now formatted, this format is:\n" +
+                            "   3.1-If there is a number in the first letter of the name of the folder of the song, all the caracter before the first space is deleted.\n" +
+                            "       (If there's no Osu! track number and the name of the artist begin with a number, it'll delete the first word of the artist name).\n" +
+                            "   3.2-Then the artist name is all the caracters before the ' - '.\n" +
+                            "   3.3-The song title is all the caracters after the ' - '.\n" +
+                            "       (If there's only one of the artist and the title, both will be the same.))\n" +
+                            "4-You click on the button Transfer and format.\n" +
+                            "5-The files (except the tutorial song that is put aside because it contains many .mp3 files and it is not a very good song to put in your library) will be copied to your destination folder with the name of the file like this :\n" +
+                            "   Artist - Title\n" +
+                            "   (Already existing files with the same name will not be replaced.)\n" +
+                            "6-The artist and song title tag will be added to the new files, the album title will be Osu!.\n" +
+                            "7-If there is a .jpg picture in the folder of the song, it'll be added to the tag of the new song file.\n" +
+                            "8-A progress bar will indicate the progression of the transfer.\n" +
+                            "9-If there are errors, they'll be in the error listbox.\n" +
+                            "10-I suggest you take a look at the files generated before adding them to your library and correct some flaws that happen because all Osu! songs don't have the same naming format."
+                             , "About Osu Song Formatter", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
